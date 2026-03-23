b2c455b6618903d595d63699abe212834b0de052
KwonJH614 <madogkwon@gmail.com>
2026-03-23 09:38:29 +0900

    refactor: @Repository 어노테이션 제거

--- src/main/java/com/example/weesh/data/jpa/unavailableDate/JpaUnavailableDateRepository.java
+++ src/main/java/com/example/weesh/data/jpa/unavailableDate/JpaUnavailableDateRepository.java
@@ -3,7 +3,6 @@ package com.example.weesh.data.jpa.unavailableDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface JpaUnavailableDateRepository extends JpaRepository<UnavailableDateEntity, Long> {
     boolean existsByDate(String date);
 }
